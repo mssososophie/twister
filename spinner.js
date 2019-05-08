c5c41af711d5db151264cea8f6f00b24ff576674
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2019-05-08 17:31:18 +0100

    adding twiterPatches array and building out the random string generator

--- spinner.js
+++ spinner.js
@@ -1,1 +1,22 @@
-testing 123
+function spin {
+
+    for
+    //loop through string
+
+
+}
+
+var randomValue = twisterPatches[Math.floor(Math.random() * myArray.length)];
+
+var patchPicked = twisterPatches[Math.floor(Math.random() * twisterPatches.length)];
+
+var twisterPatches = ['C3_016 pink', 'C3_016 orange', 'C3_016 blue',
+  'C3_017 pink', 'C3_017 organge', 'C3_017 blue',
+    'C3_018 pink', 'C3_018 organge', 'C3_018 blue',
+      'C3_021 pink', 'C3_021 organge', 'C3_021 blue',
+        'C3_022 pink', 'C3_022 organge', 'C3_022 blue',
+          'C3_023 pink', 'C3_023 organge', 'C3_023 blue',
+           'C3_024 pink', 'C3_024 organge', 'C3_024 blue',];
+
+
+console.log(spin())
